1f6e967f1a52cf78a2d1ccd8f72b6db199b22dae
Andreas Kranister <andreas.kranister@mail.fernfh.ac.at>
2026-05-01 16:35:34 +0200

    add hooks and an analyse query

---
+++ airbnb/analyses/full_moon_no_sleep.sql
@@ -0,0 +1,12 @@
+WITH mart_fullmoon_reviews AS (
+    SELECT * FROM {{ ref('mart_fullmoon_reviews') }}
+)
+SELECT
+    is_full_moon,
+    review_sentiment,
+    COUNT(*) AS reviews
+FROM mart_fullmoon_reviews
+GROUP BY
+    is_full_moon,
+    review_sentiment
+ORDER BY 1, 2
